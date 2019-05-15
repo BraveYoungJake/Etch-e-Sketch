219a6ec7dce6f55ee04815e4f57221d5e8aaf36e
Jake Young <jakemail89@gmail.com>
2019-05-15 15:52:17 -0500

    created initial grid, assigned CSS

---
+++ main.js
@@ -0,0 +1,53 @@
+///
+var wrapper = document.createElement("div")
+wrapper.classList.add("wrapper")
+document.body.appendChild(wrapper)
+
+var outerBlock = document.createElement("div")
+wrapper.classList.add("outer-block")
+wrapper.appendChild(outerBlock)
+///
+
+///
+var titleblock = document.createElement("section")
+outerBlock.appendChild(titleblock)
+
+var title = document.createElement("h1")
+title.innerHTML = "Etch-E-Sketch"
+titleblock.appendChild(title)
+
+var subTitle = document.createElement("h3")
+subTitle.innerHTML = "Hover your mouse across the box below to start drawing with Java!"
+titleblock.appendChild(subTitle)
+///
+
+///
+
+buttonContainer = document.createElement("section")
+buttonContainer.classList.add("button-container")
+outerBlock.appendChild(buttonContainer)
+
+resetButton = document.createElement("button")
+resetButton.innerHTML = "Reset Grid"
+resetButton.setAttribute("id", "reset-button")
+buttonContainer.appendChild(resetButton)
+
+colorButton = document.createElement("button")
+colorButton.innerHTML = "Change Colors"
+colorButton.setAttribute("id", "color-button")
+buttonContainer.appendChild(colorButton)
+
+///
+
+///
+var container = document.createElement("div")
+container.classList.add("container")
+outerBlock.appendChild(container)
+
+for(i=1;i <= 256; i++) {
+var gridbox = document.createElement("div")
+gridbox.classList.add("grid-box");
+gridbox.setAttribute("style", "width: 60px; height: 60px; background-color: none;")
+container.appendChild(gridbox);
+}
+///
