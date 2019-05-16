aabcf5a41d04f1717eaadafc907ca5716e1d7713
Jake Young <jakemail89@gmail.com>
2019-05-16 15:00:18 -0500

    no comment

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 ///
+let userValue = 16;
+
 var wrapper = document.createElement("div")
 wrapper.classList.add("wrapper")
 document.body.appendChild(wrapper)
@@ -44,7 +46,7 @@ var container = document.createElement("div")
 container.classList.add("container")
 outerBlock.appendChild(container)
 
-for(i=1;i <= 256; i++) {
+for(i=1;i <= (userValue* userValue); i++) {
 var gridbox = document.createElement("div")
 gridbox.classList.add("grid-box");
 gridbox.setAttribute("id", `box-${i}`)
@@ -54,17 +56,49 @@ container.appendChild(gridbox);
 ///
 
 
+const colorPool = [
+    '#7DB32B',
+    '#A41809',
+    '#421AAE',
+    '#FEFEE3',
+    '#F91A84',
+    '#D01943',
+    '#4E8669',
+    '#FBB065',
+    '#F53F42',
+    '#23B4E4',
+    '#8C5959',
+    '#D9860F',
+    '#FFAB01',
+    '#1F64EB',
+    '#71F35E',
+    '#7EAD80',
+];
+
+let colorState = {
+    color:	'#000',
+};
+
+///
+
+document.getElementById("color-button").addEventListener("click", function() {
+    
+    colorState.color = colorPool[Math.floor(Math.random() * colorPool.length)]
+
+})
+
 var myColor = document.getElementsByClassName("grid-box");
 
-for (var i = 0; i < myColor.length; i++) {
-    myColor[i].addEventListener("mouseover", function(){
-        this.style.backgroundColor = "black";
-    })
-}
+container.addEventListener('mouseover', function(e) {
+    if (e.target.classList.contains('grid-box')) {
+        e.target.style.backgroundColor = colorState.color;
+    }
+});
 
 document.getElementById("reset-button").addEventListener("click", function() {
     for (var i= 0; i < myColor.length; i++) {
-        myColor[i].style.backgroundColor = none;
+        myColor[i].style.backgroundColor = '';
     }
 })
+
 
